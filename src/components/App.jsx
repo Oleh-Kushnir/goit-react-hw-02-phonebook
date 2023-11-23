cd094f820232dcd34b4c1ba1d503912679269617
Oleh-Kushnir <olegkushnir027@gmail.com>
2023-11-23 21:45:31 +0200

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+// import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = event => {
+    const newName = event.target.value;
+    this.setState(prevState => ({
+      name: newName,
+      contacts: [...prevState.contacts, newName],
+    }));
+  };
+
+  submitChange = event => {};
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          name="name"
+          required
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <ul>
+          {this.state.contacts.map((contact, index) => (
+            <li key={index}>{contact}</li>
+          ))}
+        </ul>
+        <button type="submit" onSubmit={this.submitChange}></button>
+      </div>
+    );
+  }
+}
