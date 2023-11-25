fca205f172b52e5bcbadad8f917a5f7fbf20c43c
Oleh-Kushnir <olegkushnir027@gmail.com>
2023-11-25 14:51:39 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,17 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
-  handleChange = e => {
+  changeName = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  changeNumber = e => {
     const { name, value } = e.target;
     this.setState({
       [name]: value,
@@ -21,6 +29,7 @@ export class App extends Component {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, { ...this.state, id: nanoid() }],
       name: '',
+      number: '',
     }));
   };
 
@@ -34,8 +43,16 @@ export class App extends Component {
             type="text"
             name="name"
             required
-            onChange={this.handleChange}
+            onChange={this.changeName}
             value={this.state.name}
+          />
+          <label htmlFor="number">Name</label>
+          <input
+            type="tel"
+            name="number"
+            required
+            onChange={this.changeNumber}
+            value={this.state.number}
           />
           <button type="submit">Add Contact</button>
         </form>
@@ -44,7 +61,11 @@ export class App extends Component {
           <h1>Contacts</h1>
           <div>
             {this.state.contacts.map(el => {
-              return <div key={el.id}>{el.name}</div>;
+              return (
+                <div key={el.id}>
+                  {el.name}:{el.number}
+                </div>
+              );
             })}
           </div>
         </div>
